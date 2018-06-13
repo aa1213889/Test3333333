f3e2781e27f086d75981dbee962a9d169f43f79f
jiangzhi <jiangzhi@cqkct.com>
2018-06-13 13:58:29 +0800

    aaaa

---
+++ app/src/main/java/com/example/testtttttttt3/SharedPerferencesActivity.java
@@ -0,0 +1,13 @@
+package com.example.testtttttttt3;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SharedPerferencesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_shared_perferences);
+    }
+}
